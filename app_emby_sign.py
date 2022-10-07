e5c379bbc434c5d6438fe122dc3b129d6bbc26a9
acoolbook <72637491+acoolbook@users.noreply.github.com>
2022-10-07 12:15:21 +0800

    Update app_emby_sign.py

--- app_emby_sign.py
+++ app_emby_sign.py
@@ -70,7 +70,7 @@ async def captcha_solver(dealcap):
         #return base64data
     except:
         print_now('验证码识别平台无回应，随便返回一个结果')
-        return '67341'
+        return str(sj(10000,99999))
 
 
 

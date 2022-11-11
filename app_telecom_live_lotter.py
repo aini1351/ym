97a8d66d87777bae30f0d645628ba2b20bd65b97
acoolbook <72637491+acoolbook@users.noreply.github.com>
2022-11-11 15:03:46 +0800

    Update app_telecom_live_lotter.py

--- app_telecom_live_lotter.py
+++ app_telecom_live_lotter.py
@@ -13,15 +13,23 @@
     必须  TELECOM_PASSWORD : 电信服务密码
 3. 必须登录过 电信营业厅 app的账号才能正常运行
 """
+import requests
 from random import randint
 from re import findall
 from time import mktime, strptime
 from requests import post, get
 from datetime import datetime
 from base64 import b64encode
-
+from os import environ
 from tools.tool import timestamp, get_environ
-from china_telecom import ChinaTelecom
+from app_telecom_task import ChinaTelecom
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+phone_nums = environ.get("TELECOM_PHONE") if environ.get("TELECOM_PHONE") else ""
+foods = int(float(get_environ("TELECOM_FOOD", 10, False)))
+if phone_nums.find('\n') != -1:
+    phone_numArr = phone_nums.split('\n')
+else:
+    phone_numArr = phone_nums
 
 class TelecomLotter:
     def __init__(self, phone, password):
@@ -86,6 +94,7 @@ class TelecomLotter:
             "User-Agent": self.ua,
             "authorization": self.authorization
         }
+    
         data = post(url, headers=headers, json=body).json()
         print(data)
 
@@ -102,10 +111,30 @@ def main(phone, password):
             if 1740 > timestamp(True) - int(mktime(strptime(liveInfo["start_time"], "%Y-%m-%d %H:%M:%S"))) > 0:
                 TelecomLotter(phone, password).lotter(liveInfo["liveId"], liveInfo["period"])
 
-if __name__ == '__main__':
-    phone = get_environ("TELECOM_PHONE")
-    password = get_environ("TELECOM_PASSWORD")
-    if phone == "" or password == "":
-        print("未填写相应变量 退出")
-        exit(0)
-    main(phone, password)
+
+
+if __name__ == "__main__":
+    print('共' + str(len(phone_numArr)) + '个账户')
+    c = 0
+    l = []
+    msg = ''
+    #shuffle(phone_numArr)
+    print(phone_numArr)
+    for i in phone_numArr:
+        c = c + 1
+        print('\n账户' + str(c) + '：' + str(i) + '\n')
+        '''
+        p = threading.Thread(target=ChinaTelecom(i).main(msg), args=(i,))
+        l.append(p)
+        p.start()
+ '''
+        if '@' in i and len(i.split('@')[1]) > 4:
+            main(i.split('@')[0], i.split('@')[1])
+        else:
+            print_now('当前账户未填密码，无法抽奖，退出')
+            #msg += ChinaTelecom(i,'').main()
+
+    #send("电信app签到", msg)
+    exit(0)
+    
+
